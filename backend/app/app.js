2d91759ae8c69e24f0a2e81c774ae46daee5b0b2
firnt <lopedamo@gmail.com>
2026-07-18 16:27:58 -0300

    Añadiendo CORS y endpoints para la api de parcelas

--- backend/app/app.js
+++ backend/app/app.js
@@ -1,10 +1,14 @@
 import express from "express";
+import { endpointsDetalleParcela } from "./api/detalle_parcela.js";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const port = 8000;
 
+app.use("/api/v1/parcelas", endpointsDetalleParcela);
 app.get("/health", (req, res) => res.send("OK"));
 
 app.listen(port, () => {
