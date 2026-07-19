0ff1193d501870acab67c91040d1c2bb09802265
firnt <lopedamo@gmail.com>
2026-07-19 11:25:58 -0300

    Implementamos endpoint de tipo GET para leer informacion de la base de datos

--- backend/app/api/detalle_parcela.js
+++ backend/app/api/detalle_parcela.js
@@ -1,38 +1,32 @@
-import express from "express";
+import { Router } from "express";
+import {
+  getHistorialParcela,
+  getDatosParcela,
+} from "../db/detalle-parcelas.js";
 
-const app = express();
-app.use(express.json());
+export const endpointsDetalleParcela = Router();
 
-const port = 8000;
+// GET /api/v1/parcelas/:id 
+endpointsDetalleParcela.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  const parcela = await getDatosParcela(id);
 
-const url = 'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m';
+  if (parcela === undefined) {
+    res.sendStatus(404);
+    return;
+  }
 
-app.get("/health", (req, res) => res.send("OK"));
-
-app.listen(port, () => {
-  console.log(`todo ok`);
+  res.json(parcela);
 });
-// 1. URL de la API
-const url = 'https://open-meteo.com';
 
-// 2. Crear una función asíncrona
-async function obtenerClima() {
-    try {
-        // Esperar la respuesta de la petición HTTP
-        const response = await fetch(url);
-
-        // Esperar a que los datos se transformen en JSON
-        const data = await response.json();
-
-        // 3. Extraer los datos
-        const temperatura = data.current.temperature_2m;
-        const unidad = data.current_units.temperature_2m;
-
-        // 4. Mostrar en pantalla
-        document.getElementById('resultado').textContent = `${temperatura} ${unidad}`;
-        
-    } 
-}
-
-// 5. Ejecutar la función
-obtenerClima();
+// GET /api/v1/parcelas/:id/historial 
+endpointsDetalleParcela.get("/:id/historial", async (req, res) => {
+  const id = req.params.id;
+  const historial = await getHistorialParcela(id);
+    res.json(historial);
+    
+  if (!historial) {
+    res.status(404).json({ error: "Historial no encontrado" });
+    return;
+  }     
+});
